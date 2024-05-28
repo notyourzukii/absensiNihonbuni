099af7a855d0b47368fd3e1475672f9a1000b723
chloee <115962241+notyourzukii@users.noreply.github.com>
2024-05-28 12:58:51 +0700

    ntah

--- public/dashboard.php
+++ public/dashboard.php
@@ -16,8 +16,8 @@
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.7/css/dataTables.dataTables.css">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="shortcut icon" href="dist/image/logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="dist/style/style.css">
+    <link rel="shortcut icon" href="../dist/image/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../dist/style/style.css">
     <title>Dashboard</title>
 </head>
 
@@ -28,7 +28,7 @@
                 <i class='bx bx-chevron-right'></i>
             </div>
             <a href="" class="nav__logo">
-                <img src="dist/image/logo.png" alt="" width="20px">
+                <img src="../dist/image/logo.png" alt="" width="20px">
                 <h1 class="nav__logo-name">Rekap</h1>
             </a>
             <div class="nav__list">
@@ -36,19 +36,19 @@
                     <i class='bx bx-grid-alt'></i>
                     <span class="nav__name">Dashboard</span>
                 </a>
-                <a href="admin/home/daftarAbsen.php" class="nav__link ">
+                <a href="../admin/home/daftarAbsen.php" class="nav__link ">
                     <i class='bx bx-file'></i>
                     <span class="nav__name">Daftar Absen</span>
                 </a>
-                <a href="admin/home/daftarMember.php" class="nav__link ">
+                <a href="../admin/home/daftarMember.php" class="nav__link ">
                     <i class='bx bx-user'></i>
                     <span class="nav__name">Daftar Member</span>
                 </a>
-                <a href="admin/home/daftarIzin.php" class="nav__link">
+                <a href="../admin/home/daftarIzin.php" class="nav__link">
                     <i class='bx bx-bar-chart-square'></i>
                     <span class="nav__name">Log Absensi</span>
                 </a>
-                <a href="admin/home/settings/settings.php" class="nav__link">
+                <a href="../admin/home/settings/settings.php" class="nav__link">
                     <i class='bx bx-cog'></i>
                     <span class="nav__name">Setting</span>
                 </a>
@@ -91,7 +91,7 @@
         integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="dist/js/script.js"></script>
+    <script src="../dist/js/script.js"></script>
 </body>
 
 </html>
